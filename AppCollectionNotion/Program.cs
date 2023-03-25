6f6b11b4800f0a62ec19f6b050e8bd9e5b08fbe9
Dev205 <rodyael.205@gmail.com>
2023-03-25 20:11:12 +0300

    Add project files.

---
+++ AppCollectionNotion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppCollectionNotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
